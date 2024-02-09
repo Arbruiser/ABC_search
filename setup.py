655d292ddb12ed62ca061f57e5c9c1be73f4ea43
chaowang0524 <chaowang0524@outlook.com>
2024-02-09 13:00:43 +0200

    added "setup.py" for quick distribution

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+packages = (find_packages(exclude=["Old_Stuff*", "bk_programs*", "Arthur_website*"]),)
 
 setup(
     # Package name
+    packages=find_packages(),
     install_requires=[
         "sentence_transformers>=2.3.1",
         "scikit-learn>=1.3.1",
