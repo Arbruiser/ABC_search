2721a910e220e1a5876203746c7ce2673e7a993b
chaowang0524 <chaowang0524@outlook.com>
2024-02-09 12:49:54 +0200

    add "setup.py" for quick distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    # Package name
+    install_requires=[
+        "sentence_transformers>=2.3.1",
+        "scikit-learn>=1.3.1",
+        "numpy>=1.24.2",
+        "nltk>=3.8.1",
+        "colorama>=0.4.6",
+    ],
+)
