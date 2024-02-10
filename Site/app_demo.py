a46975d624c019b4be22e3178ce14be15a2009b7
Echo Lian <echoaces97@gmail.com>
2024-02-10 21:57:10 +0200

    wrote a sample web page, you can text "app_demo.py" on your virtual env

--- Site/app_demo.py
+++ Site/app_demo.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+# Please import database
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return render_template('test_index.html')
 
 if  __name__ == "__main__":
     app.run(debug=True)
