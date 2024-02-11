50ae9cd974fb377d8d29851a44f7f4ffb7f33740
Echo Lian <echoaces97@gmail.com>
2024-02-11 11:14:50 +0200

    update the template of return page

--- Site/app_demo.py
+++ Site/app_demo.py
@@ -3,9 +3,13 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('frontpage.html')
+
 @app.route('/')
 def index():
-    return render_template('test_index.html')
+    return render_template('returnpage.html')
 
 if  __name__ == "__main__":
     app.run(debug=True)
