06f87ab5014a434ded0776bbd4e104b6b0d3f942
Voit-Antal Artur <voitarthur@gmail.com>
2024-02-29 18:26:02 +0200

    a lot of changes to NER, minor changes to html sites

--- Site/NER_and_plotting/plotting_regular_NER.py
+++ Site/NER_and_plotting/plotting_regular_NER.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import matplotlib as mlp
 import nltk
+import json
 
 # nltk.download('stopwords') # uncomment these if you don't yet have them
 from nltk.corpus import stopwords
@@ -11,39 +12,38 @@ stop_words = set(stopwords.words("english"))
 # nltk.download('punkt')
 
 mlp.use("Agg")
-fpath = "Site/medical_document.txt"
+fpath = "Site/NER_and_plotting/NERed_recent_medical_documents.json"
 with open(fpath, "r", encoding="utf-8") as f:
-    text = f.read()
+    documents = json.load(f)
 
-# Use the nltk word_tokenize function to tokenize the text into words
 all_words = []
-for word in nltk.word_tokenize(text): # tokenizes text and then checks if tokens are words and not stop words
-    word = word.lower()
-    if (word.isalpha() and word not in stop_words): 
+for document in documents:
+    word = document["word"].lower()
+    if (word.isalpha() and word not in stop_words):  # Remove non-alphabetic tokens and stop words
         all_words.append(word)
 
 all_words_freq = nltk.FreqDist(all_words)
 
 # Get the n most common words and their frequencies
-most_common_all = dict(all_words_freq.most_common(30))
+most_common_all = dict(all_words_freq.most_common(10))
 
 # Create horizontal bar plot
 plt.figure(figsize=(10, 5))
 plt.bar(most_common_all.keys(), most_common_all.values(), color="darkorchid")
-plt.title("Word Frequency for All Documents")
+plt.title("All NER categories")
 plt.grid(axis="y", linestyle="-", linewidth=0.2)  # Adds thin horizontal gridlines
 plt.ylabel("Frequency")
 plt.xticks(rotation=45, ha="right")
 plt.subplots_adjust(bottom=0.25)
-plt.savefig(f"Site/NER_and_plotting/Plots/word_freq_no_NER_horizontal.png")
+plt.savefig(f"Site/static/Plots/word_freq_with_NER_horizontal.png")
 
 # vertical bar plot
 plt.figure(figsize=(10, 8))
 plt.barh(list(most_common_all.keys()), list(most_common_all.values()), color="darkorchid")  # can change it to vertical plot
-plt.title("Word Frequency for All Documents")
+plt.title("All NER categories")
 plt.grid(axis="x", linestyle="-", linewidth=0.2)  # Adds thin horizontal gridlines
 plt.ylabel("Frequency")
 plt.xticks(rotation=45, ha="right")
 plt.gca().invert_yaxis()  # Inverts the order
 plt.subplots_adjust(bottom=0.1)
-plt.savefig(f"Site/NER_and_plotting/Plots/word_freq_no_NER_vertical.png")
+plt.savefig(f"Site/static/Plots/word_freq_with_NER_vertical.png")
